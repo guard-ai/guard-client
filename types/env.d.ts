e2f23d735d7ba90072c978d055fa55625126e52b
Albert Terc <albytterc@protonmail.com>
2024-04-03 23:07:24 -0400

    Add types for env vars

---
+++ types/env.d.ts
@@ -0,0 +1,7 @@
+declare module '@env' {
+    export const REACT_NATIVE_SUPABASE_KEY: string;
+    export const REACT_NATIVE_SUPABASE_URL: string;
+    export const ANDROID_CLIENT_ID: string;
+    export const IOS_CLIENT_ID: string;
+    export const WEB_CLIENT_ID: string;
+}
